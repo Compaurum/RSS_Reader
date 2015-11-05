3e6df53352ae687d24d03eb4e39c976bc26b2bbe
Compaurum <compaurum@gmail.com>
2015-11-05 17:02:51 +0200

    Added Handler

--- app/src/main/java/com/example/compaurum/rss_reader/MainActivity.java
+++ app/src/main/java/com/example/compaurum/rss_reader/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.compaurum.rss_reader;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,12 +12,11 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 //import com.example.compaurum.rss_reader.adapter.ListAdapter;
+import com.example.compaurum.rss_reader.parser.Channel;
 import com.example.compaurum.rss_reader.parser.Item;
 import com.example.compaurum.rss_reader.parser.Items;
 
@@ -27,22 +27,24 @@ public class MainActivity extends ActionBarActivity{
 
     private ArrayList mFeeds = new ArrayList();  //{"Mark", "John", "Idiot"};
     private CheckBox mFavorite;
-    private Button mButton;
+    private TextView mProccess;
     private ListView mLvMain;
     private TextView channelTitle;
     private ArrayAdapter mAdapter;
+    static Handler handler = null;
+    private boolean mUpdateButtonEnabled = true;
+    public final static int START_DOWNLOADING = 1;
+    public final static int DOWNLOADING = 2;
+    public final static int END_DOWNLOADING = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //mNames.add("Mark");
-       // mNames.add("John");
-        //mNames.add("Victor");
-
         mLvMain = (ListView) findViewById(android.R.id.list);
-        mButton = (Button) findViewById(R.id.button);
+        mProccess = (TextView)findViewById(R.id.proccess);
+        //mButton = (Button) findViewById(R.id.button);
         mFavorite = (CheckBox) findViewById(R.id.checkBox1);
         channelTitle = (TextView) findViewById(R.id.channelTitle);
         mAdapter = new ArrayAdapter<>(this, R.layout.list_item, R.id.label, mFeeds);
@@ -50,21 +52,42 @@ public class MainActivity extends ActionBarActivity{
         mLvMain.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         mLvMain.setAdapter(mAdapter);
 
-        mButton.setOnClickListener(new UpdateRss(this, mLvMain));
-
+        //mButton.setOnClickListener(new UpdateRss(this, mLvMain));
         mLvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
                 Intent intent = new Intent(getApplicationContext(), ViewItem.class);
-                intent.putExtra("fullText", ((Item)mFeeds.get(position)).getFullText());
-                intent.putExtra("link", ((Item)mFeeds.get(position)).getLink());
-                intent.putExtra("date", ((Item)mFeeds.get(position)).getMpubDate());
+                intent.putExtra("fullText", ((Item) mFeeds.get(position)).getFullText());
+                intent.putExtra("link", ((Item) mFeeds.get(position)).getLink());
+                intent.putExtra("date", ((Item) mFeeds.get(position)).getMpubDate());
                 startActivity(intent);
                 Log.d("LOG_TAG", "itemClick: position = " + position + ", id = " + id);
-                Log.d("LOG_TAG", "date " + ((Item)mFeeds.get(position)).getMpubDate());
+                Log.d("LOG_TAG", "date " + ((Item) mFeeds.get(position)).getMpubDate());
 
             }
         });
+
+        handler = new Handler(){
+            public void handleMessage(android.os.Message msg) {
+                // обновляем TextView
+                switch (msg.what){
+                    case START_DOWNLOADING:
+                        mProccess.setText("Started");
+                        mUpdateButtonEnabled = false;
+                        break;
+                    case DOWNLOADING:
+                        mProccess.setText("Dowloading");
+                        break;
+                    case END_DOWNLOADING:
+                        mProccess.setText("Ended");
+                        updateList(((Channel)msg.obj).getItems());
+                        mUpdateButtonEnabled = true;
+                        break;
+                    default:
+                        break;
+                }
+            };
+        };
     }
 
     @Override
@@ -73,6 +96,13 @@ public class MainActivity extends ActionBarActivity{
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu){
+        menu.findItem(R.id.update).setEnabled(mUpdateButtonEnabled);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -84,6 +114,8 @@ public class MainActivity extends ActionBarActivity{
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }else if (id == R.id.update){
+            new UpdateRss(this, mLvMain).update();
         }
 
         return super.onOptionsItemSelected(item);
@@ -102,20 +134,13 @@ public class MainActivity extends ActionBarActivity{
     public ArrayAdapter<String> getAdapter() {
         return mAdapter;
     }
+    public static Handler getHandler() {
+        return handler;
+    }
+
     public void updateList(Items list){
         this.mFeeds.clear();
         this.mFeeds.addAll(list);
         this.mAdapter.notifyDataSetChanged();
     }
-
-    /*protected void onListItemClick(ListView l, View v, int position, long id) {
-        String item = (String) mAdapter.getItem(position);
-        Toast.makeText(this, item + " selected list Item", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        String item = (String) mAdapter.getItem(position);
-        Toast.makeText(this, item + " selected", Toast.LENGTH_SHORT).show();
-    }*/
 }
--- app/src/main/java/com/example/compaurum/rss_reader/UpdateRss.java
+++ app/src/main/java/com/example/compaurum/rss_reader/UpdateRss.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -18,47 +19,55 @@ import com.example.compaurum.rss_reader.parser.RssParser;
 /**
  * Created by compaurum on 27.10.2015.
  */
-public class UpdateRss implements View.OnClickListener {
+public class UpdateRss{
 
     private ListView mLvMain;
     private ArrayAdapter<String> mAdapter;
     private String mLink = "http://www.telegraf.in.ua/rss.xml";
     private Channel mChannel = null;
     private MainActivity mContext;
+    private Message msg;
 
     public UpdateRss(MainActivity context, ListView lvMain) {
         this.mLvMain = lvMain;
         this.mContext = context;
     }
 
-    @Override
-    public void onClick(View v) {
+    public void update() {
+        if (!isInternet()) {
+            Toast.makeText(mContext, "Turn on Internet", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mContext.getHandler().sendEmptyMessage(mContext.START_DOWNLOADING);
+                mContext.getHandler().sendEmptyMessage(mContext.DOWNLOADING);
+                RssParser rp = new RssParser(mLink);
+                rp.parse();
+                mChannel = rp.getFeed();
+                if (mChannel != null) {
+                    msg = mContext.getHandler().obtainMessage(mContext.END_DOWNLOADING, mChannel);
+                    mContext.getHandler().sendMessage(msg);
+                }
+            }
+        });
+        thread.start();
+//        try {
+//            thread.join();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        if (mChannel != null) {
+//            mContext.updateList(mChannel.getItems());
+//        }
+    }
 
+    public boolean isInternet() {
         ConnectivityManager connectivityManager = (ConnectivityManager)
                 mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected()) {
-            Thread thread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    RssParser rp = new RssParser(mLink);
-                    rp.parse();
-                    mChannel = rp.getFeed();
-                }
-            });
-            thread.start();
-
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            if (mChannel != null){
-                mContext.updateList(mChannel.getItems());
-            }
-        } else {
-            Toast.makeText(mContext, "Turn on Internet", Toast.LENGTH_SHORT).show();
-        }
+        if (networkInfo != null && networkInfo.isConnected()) return true;
+        else return false;
     }
 }
